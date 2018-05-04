db72589f99456d61f769673d6411bd66a1594758
Jarppe <jarppe@metosin.fi>
2018-05-04 08:40:00 +0300

    add some defaults

--- src/clj/app/server.clj
+++ src/clj/app/server.clj
@@ -6,8 +6,13 @@
 ;; Web server:
 ;;
 
+(def +default-opts+ {:host "localhost"
+                     :port 3000
+                     :path "/"})
+
 (defmethod ig/init-key ::server [_ {:keys [handler] :as opts}]
-  (immutant/run handler (dissoc opts :handler)))
+  (immutant/run handler (-> (merge +default-opts+ opts)
+                            (dissoc :handler))))
 
 (defmethod ig/halt-key! ::server [_ server]
   (immutant/stop server))
